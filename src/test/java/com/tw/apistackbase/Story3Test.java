647ea71773eb8969372666a24a29ff43dd631e57
Ethan <ethan.huang@oocl.com>
2019-07-17 23:25:19 +0800

    Feat: completed Story3.2 AC2 test

--- src/test/java/com/tw/apistackbase/Story3Test.java
+++ src/test/java/com/tw/apistackbase/Story3Test.java
@@ -2,6 +2,7 @@ package com.tw.apistackbase;
 
 import com.tw.apistackbase.entity.CaseInfo;
 import com.tw.apistackbase.entity.LawCase;
+import com.tw.apistackbase.entity.Procurator;
 import com.tw.apistackbase.entity.Procuratorate;
 import com.tw.apistackbase.repository.LawCaseRepository;
 import org.junit.Test;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -25,9 +27,9 @@ public class Story3Test {
     @Autowired
     LawCaseRepository lawCaseRepository;
 
-    //Story3-AC2
+    //Story3.1-AC2
     @Test
-    public void should_return_specific_case_info_when_call_find_by_id_given_law_case_id(){
+    public void should_return_specific_case_info_when_call_find_by_id_given_law_case_id_1(){
         Long time = Long.valueOf(20190717);
         Procuratorate procuratorate = new Procuratorate("yeshila");
         LawCase lawCase = new LawCase("Garson",time,procuratorate);
@@ -36,9 +38,9 @@ public class Story3Test {
         assertEquals(procuratorate2,procuratorate);
     }
 
-    //Story2-AC4
+    //Story3.1-AC4
     @Test
-    public void should_add_case_info_description_to_law_case_when_call_find_all(){
+    public void should_add_case_info_description_to_law_case_when_call_find_all_1(){
         Long time1 = Long.valueOf(20190717);
         Long time2 = Long.valueOf(20190203);
         Procuratorate procuratorate1 = new Procuratorate("yeshila");
@@ -54,9 +56,27 @@ public class Story3Test {
             LawCase lawCase = lawCaseRepository.findAll().get(i);
             lawCase.setProDescription(lawCase.getProcuratorate().getName());
         }
-//        List<LawCase> lawCases = lawCaseRepository.findAll();
 
         assertThat(lawCase3.toString1(),is(lawCase1.toString1()));
         assertThat(lawCase4.toString1(),is(lawCase2.toString1()));
     }
+
+    //Story3.2-AC2
+    @Test
+    public void should_return_specific_case_info_when_call_find_by_id_given_law_case_id_2(){
+        Long time = Long.valueOf(20190717);
+        Procurator procurator1 = new Procurator("liangfeifan");
+        Procurator procurator2 = new Procurator("zhazhahui");
+        List<Procurator> procuratorList = new ArrayList<>();
+
+        procuratorList.add(procurator1);
+        procuratorList.add(procurator2);
+        Procuratorate procuratorate = new Procuratorate("yeshila",procuratorList);
+        LawCase lawCase = new LawCase("Garson",time,procuratorate);
+        lawCaseRepository.save(lawCase);
+        Procuratorate procuratorate2 = lawCaseRepository.findById(lawCase.getId()).get().getProcuratorate();
+        assertEquals(procuratorate2,procuratorate);
+    }
+
+    
 }
